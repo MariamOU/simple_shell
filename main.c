98b72c34a92029459e79e28fb521a029470f480c
MariamOU <marymyara@gmail.com>
2023-11-16 16:24:18 -0800

    creation of shell.h and main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/**
+ * main - The main function
+ * @ac: the counter of arguments
+ * @av: the arguments
+ * Description: program uses the main function
+ * Return: 0;
+ */
+
+int main(int ac, char **av)
+{
+(void)ac;
+(voi)av;
+
+return (0);
+}
