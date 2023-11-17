81133dabc0f6ad67086f9bc792981144a71b3e6b
MariamOU <marymyara@gmail.com>
2023-11-16 16:30:54 -0800

    main.c

--- main.c
+++ main.c
@@ -11,6 +11,19 @@ int main(int ac, char **av)
 {
 (void)ac;
 (voi)av;
+char *line = NULL;
+size_t line_length = 0;
+ssize_t num_characters = 0;
+int i = 0;
+
+write(STDOUT_FILENO, "$ ", 2);
+num_characters = getline(&line, &line_length, stdin);
+if (num_characters == EOF)
+{
+perror("getline error");
+exit(EXIT_FAILURE);
+}
+printf("%s ", line);
 
 return (0);
 }
